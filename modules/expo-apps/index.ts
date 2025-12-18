36e19891460304dce78752d9f391dcd65559edff
iuuukhueeee <nguyenducthien9@gmail.com>
2025-12-18 09:10:27 +0700

    feat: clean the boilerplate

--- modules/expo-apps/index.ts
+++ modules/expo-apps/index.ts
@@ -1,5 +1,5 @@
 // Reexport the native module. On web, it will be resolved to ExpoAppsModule.web.ts
 // and on native platforms to ExpoAppsModule.ts
+export * from './src/ExpoApps.types';
 export { default } from './src/ExpoAppsModule';
-export { default as ExpoAppsView } from './src/ExpoAppsView';
-export * from  './src/ExpoApps.types';
+
--- modules/expo-apps/src/ExpoAppsModule.ts
+++ modules/expo-apps/src/ExpoAppsModule.ts
@@ -1,12 +1,8 @@
-import { NativeModule, requireNativeModule } from 'expo';
+import { NativeModule, requireNativeModule } from "expo";
 
-import { ExpoAppsModuleEvents } from './ExpoApps.types';
+import { ExpoAppsModuleEvents } from "./ExpoApps.types";
 
-declare class ExpoAppsModule extends NativeModule<ExpoAppsModuleEvents> {
-  PI: number;
-  hello(): string;
-  setValueAsync(value: string): Promise<void>;
-}
+declare class ExpoAppsModule extends NativeModule<ExpoAppsModuleEvents> {}
 
 // This call loads the native module object from the JSI.
-export default requireNativeModule<ExpoAppsModule>('ExpoApps');
+export default requireNativeModule<ExpoAppsModule>("ExpoApps");
