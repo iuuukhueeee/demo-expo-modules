d4816beff248e5eacdd305e5661e53d0146e2efe
iuuukhueeee <nguyenducthien9@gmail.com>
2025-12-18 09:46:01 +0700

    feat: add authorizeAsync

--- modules/expo-apps/src/ExpoAppsModule.ts
+++ modules/expo-apps/src/ExpoAppsModule.ts
@@ -1,8 +1,10 @@
 import { NativeModule, requireNativeModule } from "expo";
 
-import { ExpoAppsModuleEvents } from "./ExpoApps.types";
-
-declare class ExpoAppsModule extends NativeModule<ExpoAppsModuleEvents> {}
+declare class ExpoAppsModule extends NativeModule {
+  // using the same name as we defined in ExpoAppModule.swift
+  // The function is an async function so we return a Promise
+  authorizeAsync: () => Promise<void>;
+}
 
 // This call loads the native module object from the JSI.
 export default requireNativeModule<ExpoAppsModule>("ExpoApps");
